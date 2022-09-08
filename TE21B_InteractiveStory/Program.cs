1d8299dd5f54c476abcf891a035ad312dc1c6f93
Maximilian Persson <Maximilian.persson2@elev.ga.ntig.se>
2022-09-08 14:27:42 +0200

    klar

---
+++ TE21B_InteractiveStory/Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+    Console.WriteLine("Hello, choose your name Luck or Nexus");
+
+    string name = Console.ReadLine();
+    // name = name.ToLower();
+
+    Console.WriteLine($"Hello {name}");
+
+
+    if (name.ToLower() == "luck")
+    {
+
+        Console.WriteLine("Luck your luck has ended!");
+        
+        
+
+    }
+    else if (name.ToLower() == "nexus")
+    {
+        Console.WriteLine("A wise choice and personally my favourite one");
+
+        Console.WriteLine("save yourself or save drowning kid");
+
+
+        string save = Console.ReadLine();
+
+        if (save == "save yourself")
+        {
+            Console.WriteLine("You got stabbed from behind");
+        }
+        else if (save == "save drowning kid")
+        {
+            Console.WriteLine("You both survived");
+            Console.WriteLine("Congrats you made it to the end");
+        }
+
+
+
+
+
+
+
+
+    }
+
+    else
+    {
+
+    }
+    Console.ReadLine();
+    
+
